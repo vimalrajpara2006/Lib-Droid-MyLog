f3abab03e3e7ac07a9c4626333104e30575ff621
Vimal Rajpara <vimalrajpara2006@gmail.com>
2013-08-05 16:14:23 +0530

    First commit in this library repo

---
+++ src/om/we7aura/library/log/MyLog.java
@@ -0,0 +1,34 @@
+package om.we7aura.library.log;
+
+public class MyLog {
+	// TODO set false in below variable "LOG" for market upload
+    static boolean LOG = true;
+
+    public static boolean isLOG() {
+		return LOG;
+	}
+	public static void setLOG(boolean lOG) {
+		LOG = lOG;
+	}
+	public static void i(String tag, String string) {
+        if (LOG) android.util.Log.i(tag, string);
+    }
+    public static void e(String tag, String string) {
+        if (LOG && (!tag.equalsIgnoreCase("imageloader"))) android.util.Log.e(tag, string);
+    }
+    public static void d(String tag, String string) {
+        if (LOG) android.util.Log.d(tag, string);
+    }
+    public static void v(String tag, String string) {
+        if (LOG) android.util.Log.v(tag, string);
+    }
+    public static void w(String tag, String string) {
+        if (LOG) android.util.Log.w(tag, string);
+    }
+	public static void e(String tag, String string, Exception e) {
+		if (LOG && (!tag.equalsIgnoreCase("imageloader"))) android.util.Log.e(tag, string+" : "+e.getMessage());
+	}
+	public static void function(String string) {
+        if (LOG) android.util.Log.i("Function", string);
+    }
+}
